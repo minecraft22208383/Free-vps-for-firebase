2d80e4953827999295e1b1151b3adfed2a712a26
minecraft22208383 <theminematers@gmail.com>
2025-09-29 14:10:07 +0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,40 @@
+clear
+cd
+echo "Downloading Ubuntu...."
+wget http://cdimage.ubuntu.com/ubuntu-base/releases/20.04/release/ubuntu-base-20.04.4-base-amd64.tar.gz -O ubuntu.tar.gz
+echo "Extracting Ubuntu"
+mkdir ubuntu
+tar -xf ubuntu.tar.gz -C ubuntu &> /dev/null
+rm ubuntu.tar.gz
+echo "Downloading Proot"
+wget -O proot "https://downloads.sourceforge.net/project/proot.mirror/v5.3.0/proot-v5.3.0-x86_64-static?ts=gAAAAABo2hXN2r2g99TJNV9gPdRhsBvKOupjqoVyyL5FD9-YabatbTwIAbd-yCFeLO5AfqWkb3PeBgQ1mhHw2e94gppk0IZ2FQ%3D%3D&use_mirror=master&r="
+chmod +x proot
+echo "cd && ./proot -0 -r ubuntu -b /sys -b /proc -b /dev -w /root  /usr/bin/env -i HOME=/root  PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin TERM=$TERM LANG=C.UTF-8 /bin/bash" >> start
+chmod +x start
+cat >> "ubuntu/etc/resolv.conf" << EOF
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+EOF
+cd ubuntu/root
+rm -rf .bashrc
+cat >> ".bashrc" << EOF
+apt update 
+echo "Downloading Qemu"
+apt install qemu-system-x86 qemu-utils wget -y
+cd /opt
+mkdir windows
+cd windows
+wget https://github.com/L36D/Tiny10/releases/download/v1/Tiny10.iso
+qemu-img create -f raw windows.img 11G
+cd
+rm -rf .bashrc
+exit
+EOF
+cd 
+./start
+cd ubuntu/root
+cat >> ".bashrc" << EOF
+echo "Windows is Booting"
+cd /opt/windows
+qemu-system-x86_64 -m 2G   -smp 4   -cpu max  -accel tcg,thread=multi -hda windows.img -cdrom Tiny10.iso -device e1000,netdev=net0 -netdev user,id=net0  -vnc :1    
+EOF
